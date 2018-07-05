b39faf03be5d29594ece830434e8b74a93c57a12
ahmed-sallam <devahmedsallam@gmail.com>
2018-07-05 09:09:54 +0200

    updates

--- app/app.js
+++ app/app.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const data = require('./data/data.json');
 
 const app = express();
+app.set('port', process.env.PORT || 3000);
 app.get('/', (req, res)=>{
-    res.send('<h1>Hello world</h1>');
+    res.send(`
+        <h1>Hello world</h1>
+    `);
+});
+app.get('/speakers', (req, res)=>{
+    let info='';
+    data.speakers.forEach((item)=>{
+       info += `<li>
+            <h2>${item.name}</h2>
+            <p>${item.summary}</p>
+        </li>
+        `
+    });
+    res.send(`
+        ${info}
+    `);
+});
+app.get('/speakers/:speakerId', (req, res)=>{
+    let speaker = data.speakers[req.params.speakerId];
+    res.send(`
+        <h1>${speaker.title}</h1>
+        <h2>${speaker.name}</h2>
+        <p>${speaker.summary}</p>
+    `);
 });
 
-app.listen(3000, ()=>{
-    console.log('server getting start with port 3000');
+app.listen(app.get('port'), ()=>{
+    console.log(`server getting start with port ${app.get('port')}`);
 });
